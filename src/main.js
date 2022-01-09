d36edb546abb49ac95e13c6fec9c4a0fc0165c2f
Tober <1946433187@qq.com>
2022-01-10 01:55:19 +0800

    项目搭建

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+// 引入路由
+import router from "@/router";
+import store from '@/store'
 
-Vue.config.productionTip = false
+import "@/assets/styles/resets.css";
+import "@/assets/styles/border.css";
+import "@/assets/styles/common.scss";
+import "@/assets/js/fastclick.js";
+import "@/assets/js/common.js";
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+  // 注册路由
+  router,
+  store
+}).$mount("#app");
