1d2f3af3e96ebe716b189fe83140ced5cecf516d
Tober <1946433187@qq.com>
2022-01-11 23:43:50 +0800

    路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,12 @@
 import Vue from "vue";
 import Router from "vue-router";
-import HelloWorld from "@/components/HelloWorld";
+
+// components
+import Index from "@/pages/Index";
+import List from "@/pages/List";
+import Detail from "@/pages/Detail";
+import City from "@/pages/City";
+import Search from "@/pages/Search";
 
 Vue.use(Router);
 
@@ -8,8 +14,28 @@ export default new Router({
   routes: [
     {
       path: "/",
-      name: HelloWorld,
-      component: HelloWorld,
+      name: "Index",
+      component: Index,
+    },
+    {
+      path: "/list",
+      name: "List",
+      component: List,
+    },
+    {
+      path: "/detail",
+      name: "Detail",
+      component: Detail,
+    },
+    {
+      path: "/city",
+      name: "City",
+      component: City,
+    },
+    {
+      path: "/search",
+      name: "Search",
+      component: Search,
     },
   ],
 });
