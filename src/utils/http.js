6f6de55d0b9556d448a43f98de28ef160e718dbf
Tober <1946433187@qq.com>
2022-01-11 23:41:52 +0800

    API配置, axios封装

---
+++ src/utils/http.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import qs from "qs";
+
+class HTTP {
+  axiosPost(options) {
+    axios({
+      url: options.url,
+      method: "post",
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      data: qs.stringify(options.data),
+    })
+      .then((res) => {
+        options.success(res);
+      })
+      .catch((err) => {
+        options.error(err);
+      });
+  }
+}
+
+export { HTTP };
