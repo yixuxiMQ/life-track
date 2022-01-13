0087c0ce576f797dd63be3ddb761c2dbacf0b47d
Tober <1946433187@qq.com>
2022-01-14 01:15:23 +0800

    公共header组件

---
+++ src/components/Header/Common.vue
@@ -0,0 +1,38 @@
+<template>
+  <header class="header">
+    <back-ward></back-ward>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+import BackWard from "./Sub/BackWard.vue";
+export default {
+  name: "CommonHeader",
+  props: {
+    title: {
+      type: String,
+      default() {
+        return "生活轨迹";
+      },
+    },
+  },
+  components: {
+    BackWard,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "styles/mixins.scss";
+@import "styles/variables.scss";
+
+.header {
+  position: relative;
+  height: $headerHeight;
+  background-color: $defaultBgColor;
+  @include flex-row;
+  @include vh-center;
+  font-size: 0.18rem;
+}
+</style>
