19f9234d6416d29f37849ebd9caed39d4a9dba9b
Tober <1946433187@qq.com>
2022-01-14 01:21:07 +0800

    其他

---
+++ src/components/Header/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="header">
+    <city-selector></city-selector>
+    <false-input></false-input>
+  </header>
+</template>
+
+<script>
+import CitySelector from "./Sub/CitySelector.vue";
+import FalseInput from './Sub/FalseInput.vue'
+export default {
+  name: "HomeHeader",
+  components: {
+    CitySelector,
+    FalseInput
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "styles/mixins.scss";
+@import "styles/variables.scss";
+
+.header {
+  @include flex-row;
+  height: $headerHeight;
+  background-color: $defaultBgColor;
+}
+</style>
