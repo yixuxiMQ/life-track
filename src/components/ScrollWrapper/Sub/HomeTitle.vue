018f1c561e548751d2b54f245b7d9036759b8754
Tober <1946433187@qq.com>
2022-01-19 03:16:33 +0800

    HomeTitle/Stars组件

---
+++ src/components/ScrollWrapper/Sub/HomeTitle.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home-title">
+    <p>{{ title }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeTitle",
+  props: {
+    title: {
+      type: String,
+      default() {
+        return "推荐列表";
+      },
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.home-title {
+  display: flex;
+  align-items: center;
+  height: 0.5rem;
+  margin-top: 0.1rem;
+  background-color: #fff;
+  padding-left: 0.15rem;
+  border-bottom: 1px solid #ddd;
+  box-sizing: border-box;
+
+  p {
+    font-size: 0.18rem;
+    color: #000;
+  }
+}
+</style>
