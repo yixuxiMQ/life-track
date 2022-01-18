a5e2b24857029651e77033e8d15698981fc1cb59
Tober <1946433187@qq.com>
2022-01-19 03:14:01 +0800

    定义getHomeDatas函数

--- src/models/index.js
+++ src/models/index.js
@@ -1,6 +1,27 @@
 import { HTTP } from "@/utils/http";
 import config from "@/utils/config";
 
-class IndexModel extends HTTP {}
+class IndexModel extends HTTP {
+  getHomeDatas(cityId) {
+    return new Promise((resolve) => {
+      this.axiosPost({
+        url: config.API.GET_HOME_DATAS,
+        data: cityId,
+        success(data) {
+          resolve({
+            data,
+            status: 0,
+          });
+        },
+        error(err) {
+          resolve({
+            err,
+            status: -1,
+          });
+        },
+      });
+    });
+  }
+}
 
 export { IndexModel };
