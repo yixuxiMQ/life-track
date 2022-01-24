35a99ae2961ba694b77227e00385faf1b4ee3a90
Tober <1946433187@qq.com>
2022-01-24 18:16:41 +0800

    推荐KTV列表组件

---
+++ src/components/ScrollWrapper/KtvList/Index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="ktv-wrapper">
+    <ktv-list-sub
+      v-for="item in ktvList"
+      :key="item.id"
+      :data="item"
+    ></ktv-list-sub>
+  </div>
+</template>
+
+<script>
+import KtvListSub from "./Sub.vue";
+export default {
+  name: "KtvList",
+  props: {
+    ktvList: Array,
+  },
+  components: {
+    KtvListSub,
+  },
+};
+</script>
+
+<style></style>
