654973539870370649e1c0b157f462fb3f3efaaa
Tober <1946433187@qq.com>
2022-01-26 19:36:23 +0800

    Swiper组件

---
+++ src/components/ScrollWrapper/Sub/Swiper.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="swiper-wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="(item, index) of picDatas" :key="index">
+        <img :src="item" class="swiper-img" />
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+import { swiper, swiperSlide } from "vue-awesome-swiper";
+import "swiper/dist/css/swiper.css";
+
+export default {
+  name: "DetailSwiper",
+  props: {
+    picDatas: Array,
+  },
+  components: {
+    swiper,
+    swiperSlide,
+  },
+  data() {
+    return {
+      swiperOption: {
+        pagination: {
+          el: ".swiper-pagination",
+        },
+      },
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "styles/variables.scss";
+
+.swiper-wrapper {
+  & /deep/ .swiper-pagination-bullet-active {
+    background-color: $defaultGreen;
+  }
+}
+.swiper-slide {
+  height: 2rem;
+}
+
+.swiper-img {
+  width: 100%;
+  height: 100%;
+}
+</style>
