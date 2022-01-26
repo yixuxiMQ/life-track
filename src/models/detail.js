cf82479fc029e1b08b69fd57806f930d99c53d3d
Tober <1946433187@qq.com>
2022-01-26 18:51:17 +0800

    DetailModel

--- src/models/detail.js
+++ src/models/detail.js
@@ -1,6 +1,29 @@
 import { HTTP } from "@/utils/http";
 import config from "@/utils/config";
 
-class DetailModel extends HTTP {}
+class DetailModel extends HTTP {
+  getDetail(field, id) {
+    return new Promise((resolve) => {
+      this.axiosPost({
+        url: config.API.GET_DETAIL,
+        data: {
+          field,
+          id,
+        },
+        success(data) {
+          resolve({
+            data,
+            status: 0,
+          });
+        },
+        error() {
+          resolve({
+            status: -1,
+          });
+        },
+      });
+    });
+  }
+}
 
 export { DetailModel };
