bfdc7127eff17461a9368e38720501a2b02b1e40
Tober <1946433187@qq.com>
2022-01-28 02:31:07 +0800

    DetailOpenDateTime组件

---
+++ src/components/ScrollWrapper/Detail/Sub/OpenDateTime.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="datetime-wrapper">
+    <span class="iconfont icon-datetime"></span>
+    <span class="datetime-text">{{ dateTime }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DetailOpenDateTime",
+  props: {
+    dateTime: {
+      type: String,
+      default() {
+        return "加载中...";
+      },
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.datetime-wrapper {
+  display: flex;
+  height: 0.44rem;
+  align-items: center;
+  padding: 0.03rem 0.15rem;
+  background-color: #fff;
+  border-bottom: 1px solid #ddd;
+  box-sizing: border-box;
+
+  .icon-datetime {
+    font-size: 0.16rem;
+    color: #999;
+    margin-right: 0.15rem;
+  }
+
+  .datetime-text {
+    font-size: 0.14rem;
+  }
+}
+</style>
