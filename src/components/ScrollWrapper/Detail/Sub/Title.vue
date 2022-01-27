3e0776e956104f1e5c5eed2c7b3ea9bd16ef0291
Tober <1946433187@qq.com>
2022-01-28 02:31:49 +0800

    DetailTitle组件

---
+++ src/components/ScrollWrapper/Detail/Sub/Title.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="title-wrapper">
+    <h1>
+      <span :class="['iconfont', icon]"></span>
+      <span class="title">{{ title }}</span>
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DetailTitle",
+  props: {
+    icon: String,
+    title: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "styles/variables.scss";
+.title-wrapper {
+  display: flex;
+  height: 0.44rem;
+  align-items: center;
+  line-height: 0.44rem;
+  padding: 0 0.15rem;
+  background-color: #fff;
+  border-bottom: 1px solid #ddd;
+  box-sizing: border-box;
+  color: $defaultGreen;
+
+  .iconfont {
+    font-size: 0.18rem;
+    margin-right: 0.1rem;
+  }
+
+  .title {
+    font-size: 0.14rem;
+  }
+}
+</style>
