c4e685f1d34f7ddae93f38d5a7110741cdeaf666
Tober <1946433187@qq.com>
2022-01-28 02:30:16 +0800

    DetailAddress组件

---
+++ src/components/ScrollWrapper/Detail/Sub/Address.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="address-wrapper">
+    <span class="iconfont icon-positioning"></span>
+    <span class="address-text">{{ address }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DetailAddress",
+  props: {
+    address: {
+      type: String,
+      default() {
+        return "加载中...";
+      },
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.address-wrapper {
+  display: flex;
+  align-items: center;
+  min-height: 0.44rem;
+  padding: 0.1rem 0.15rem;
+  background-color: #fff;
+  border-bottom: 1px solid #ddd;
+  box-sizing: border-box;
+
+  .icon-positioning {
+    font-size: 0.18rem;
+    margin-right: 0.1rem;
+    color: #999;
+  }
+
+  .address-text {
+    font-size: 0.14rem;
+    line-height: 0.18rem;
+  }
+}
+</style>
