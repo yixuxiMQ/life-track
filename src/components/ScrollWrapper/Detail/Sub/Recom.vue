45a1fb9693b9d52675ad496d4970968e99698a2f
Tober <1946433187@qq.com>
2022-01-28 02:32:12 +0800

    DetailRecom组件

---
+++ src/components/ScrollWrapper/Detail/Sub/Recom.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="recom-wrapper">
+    <detail-title title="推荐菜品" icon="icon-recom"></detail-title>
+    <div class="recom-content">{{ recom }}</div>
+  </div>
+</template>
+
+<script>
+import DetailTitle from "./Title.vue";
+export default {
+  name: "DetailRecom",
+  props: {
+    recom: {
+      type: String,
+      default() {
+        return "加载中...";
+      },
+    },
+  },
+  components: {
+    DetailTitle,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "styles/mixins.scss";
+
+.recom-wrapper {
+  @include flex-column;
+  margin-top: 0.1rem;
+  background-color: #fff;
+  border-bottom: 1px solid #ddd;
+  box-sizing: border-box;
+
+  .recom-content {
+    padding: 0.1rem 0.15rem;
+    font-size: 0.14rem;
+    line-height: 0.18rem;
+    box-sizing: border-box;
+    text-indent: 2em;
+  }
+}
+</style>
