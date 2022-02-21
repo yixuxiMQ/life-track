49ada320e8afea9e7a60379cb511d94be97daf9e
Tober <1946433187@qq.com>
2022-02-22 03:29:59 +0800

    列表页Tab组件

---
+++ src/components/Tab/Index.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav class="tab">
+    <tab-sub
+      v-for="(item, index) in cityDatas"
+      :key="index"
+      :cityInfo="item"
+    ></tab-sub>
+  </nav>
+</template>
+
+<script>
+import TabSub from "./Sub.vue";
+import cityDatas from "data/cities.js";
+
+export default {
+  name: "Tab",
+  components: {
+    TabSub,
+  },
+  data() {
+    return {
+      cityDatas,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import 'styles/mixins.scss';
+
+.tab{
+    @include flex-row;
+    height: .44rem;
+    flex-wrap: nowrap;
+    background-color: #fff;
+    border-bottom: 1px solid #ddd;
+}
+</style>
