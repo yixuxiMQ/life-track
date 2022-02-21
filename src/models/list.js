487e4b09e59b93dbc6b9278ca1867ebe5b7cf0b5
Tober <1946433187@qq.com>
2022-02-22 03:36:13 +0800

    网络测试

--- src/models/list.js
+++ src/models/list.js
@@ -1,6 +1,30 @@
 import { HTTP } from "@/utils/http";
 import config from "@/utils/config";
 
-class ListModel extends HTTP {}
+class ListModel extends HTTP {
+  getListDatas(cityId, field) {
+    return new Promise((resolve) => {
+      this.axiosPost({
+        url: config.API.GET_LIST_DATAS,
+        data: {
+          cityId,
+          field,
+        },
+        success(data) {
+          resolve({
+            data,
+            status: 0,
+          });
+        },
+        error(err) {
+          resolve({
+            err,
+            status: -1,
+          });
+        },
+      });
+    });
+  }
+}
 
 export { ListModel };
