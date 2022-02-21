6e95f296c9d871fa847fc679f13c42e82619b69b
Tober <1946433187@qq.com>
2022-02-22 03:30:53 +0800

    列表页Loading组件

---
+++ src/components/ScrollWrapper/Sub/Loading.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="loading" v-show="loadingShow">
+    <img class="loading-img" src="~images/loading.gif" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Loading",
+  props: {
+    loadingShow: {
+      type: Boolean,
+      default() {
+        return true;
+      },
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "styles/mixins.scss";
+
+.loading {
+  @include vh-center;
+  height: 3rem;
+
+  .loading-img {
+    width: 0.8rem;
+    height: 0.8rem;
+  }
+}
+</style>
