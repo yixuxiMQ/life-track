09f36924ce265dabdb5d7823e4c27c40dad840b5
Tober <1946433187@qq.com>
2022-02-23 01:05:56 +0800

    SearchModel数据模型

--- src/models/search.js
+++ src/models/search.js
@@ -1,6 +1,30 @@
 import { HTTP } from "@/utils/http";
 import config from "@/utils/config";
 
-class SearchModel extends HTTP {}
+class SearchModel extends HTTP {
+  searchAction(keyword, cityId) {
+    return new Promise((resolve) => {
+      this.axiosPost({
+        url: config.API.SEARCH_ACTION,
+        data: {
+          keyword,
+          cityId,
+        },
+        success(data) {
+          resolve({
+            data,
+            status: 0,
+          });
+        },
+        error(err) {
+          resolve({
+            err,
+            status: -1,
+          });
+        },
+      });
+    });
+  }
+}
 
 export { SearchModel };
