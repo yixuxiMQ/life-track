0e9f7738f2836d0f4766212cfc49796a89a54b86
Tober <1946433187@qq.com>
2022-02-23 01:03:16 +0800

    NoDataTip组件

---
+++ src/components/ScrollWrapper/Sub/NoDataTip.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="no-data-tip">
+    <span class="text">{{ noDataTip }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NoDataTip",
+  data() {
+    return {
+      noDataTip: "- 没有搜索到相关结果 -",
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "styles/mixins.scss";
+
+.no-data-tip {
+  @include vh-center;
+  height: 3rem;
+
+  .text {
+    font-size: 0.16rem;
+    color: #999;
+  }
+}
+</style>
