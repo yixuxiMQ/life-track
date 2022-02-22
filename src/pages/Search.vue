a1dac2f1a3ab76ad4dc7d7d44dc0dfdc10ea51c5
Tober <1946433187@qq.com>
2022-02-23 01:07:27 +0800

    添加子组件及数据处理

--- src/pages/Search.vue
+++ src/pages/Search.vue
@@ -1,21 +1,43 @@
 <template>
   <div class="container">
     <common-header :title="title"></common-header>
+    <search-input @onSearch="onSearch"></search-input>
+    <search-scroll-wrapper :data="data"></search-scroll-wrapper>
   </div>
 </template>
 
 <script>
-import CommonHeader from 'components/Header/Common.vue'
+import CommonHeader from "components/Header/Common.vue";
+import SearchInput from "components/SearchInput/Index.vue";
+import SearchScrollWrapper from "components/ScrollWrapper/Search.vue";
+
+import tools from "@/utils/tools";
+
 export default {
-  name: 'SearchPage',
+  name: "SearchPage",
   components: {
-    CommonHeader
+    CommonHeader,
+    SearchInput,
+    SearchScrollWrapper,
   },
-  data () {
+  data() {
     return {
-      title: '搜索页面'
-    }
-  }
+      title: "商家搜索",
+      data: {},
+    };
+  },
+  methods: {
+    onSearch(res) {
+      if (res && res.status === 0) {
+        const { data } = res;
+        data.foodDatas = tools.formatJSON(data.foodDatas, "keyword");
+        this.data = data;
+      } else {
+        this.data = res;
+        console.log(this.data);
+      }
+    },
+  },
 };
 </script>
 
