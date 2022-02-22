391a7237c97b6b138e2f648325f6b079b49c9484
Tober <1946433187@qq.com>
2022-02-23 01:02:17 +0800

    trimSpace/throttle工具函数

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -19,4 +19,30 @@ export default {
   replaceToSpace(str) {
     return str.replace(/,/g, " ");
   },
+
+  trimSpace(str) {
+    return str.replace(/\s+/, "");
+  },
+
+  throttle(fn, delay) {
+    var t = null,
+      begin = new Date().getTime();
+
+    return function () {
+      var _self = this,
+        args = arguments,
+        cur = new Date().getTime();
+
+      clearTimeout(t);
+
+      if (cur - begin >= delay) {
+        fn.apply(_self, args);
+        begin = cur;
+      } else {
+        t = setTimeout(function () {
+          fn.apply(_self, args);
+        }, delay);
+      }
+    };
+  },
 };
